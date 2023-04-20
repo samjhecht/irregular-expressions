7e9f19de02e22c64ebc26b88e8e734f4d3cbc01d
samjhecht@gmail.com <samjhecht@gmail.com>
2023-04-19 17:52:34 -0700

    removing google tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,12 @@ import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import './global-styles.css';
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
+import { Analytics } from '@vercel/analytics/react';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-const isProd = process.env.NODE_ENV === "production";
-
+// const isProd = process.env.NODE_ENV === "production";
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -23,21 +23,21 @@ export interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      if (isProd) {
-        // window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
-        //   page_path: url,
-        // });
-        window && window.gtag && window.gtag('config', process.env.NEXT_PUBLIC_GA_ID as string, { page_path: url, }); 
-      }
-    }
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    }
-  }, [router.events]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const handleRouteChange = (url: URL) => {
+  //     if (isProd) {
+  //       // window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
+  //       //   page_path: url,
+  //       // });
+  //       window && window.gtag && window.gtag('config', process.env.NEXT_PUBLIC_GA_ID as string, { page_path: url, }); 
+  //     }
+  //   }
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //   }
+  // }, [router.events]);
 
   return (
     <CacheProvider value={emotionCache}>
@@ -48,6 +48,7 @@ export default function MyApp(props: MyAppProps) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+         <Analytics />
       </ThemeProvider>
     </CacheProvider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,32 +17,12 @@ interface MyDocumentProps extends DocumentProps {
   emotionStyleTags: JSX.Element[];
 }
 
-const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
   return (
     <Html lang="en" className={myFonts.className}>
       <Head>
-        {/* Google Analytics Measurement ID*/}
-        {isProd && (
-          <>
-            <script async src={gtag} />
-            {/ Inject the GA tracking code with the Measurement ID /}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-                  page_path: window.location.pathname
-                });
-              `,
-              }}
-            />
-          </>
-        )}
         {/* PWA primary color */}
         <link rel="alternate" type="application/rss+xml" title="irregular-expressions.com rss feed" href="/rss.xml" />
         <meta name="theme-color" content={theme.palette.primary.main} />
