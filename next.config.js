aee85facc498251694a6cdbfcd375d85a9a53230
samjhecht@gmail.com <samjhecht@gmail.com>
2023-04-26 14:06:49 -0700

    issue with contentlayer

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.resolve.alias['next/dist/client/dev/error-overlay/websocket.js'] = 'next/dist/client/next.js';
+    }
+
+    return config;
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
