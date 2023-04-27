5cfeee9bdb2e9e1c8d12b32f16523946ac574fc8
samjhecht@gmail.com <samjhecht@gmail.com>
2023-04-27 11:02:33 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Irregular Expressions with NextJs and Typescript
 
 # TODO:
-- sitemap
 - go back to using next/image to improve loading optimization
 - database for login
 - roll my own aggregation feed that has a card for every poem, essay, or instagram post (and source them from insta)
--- next.config.js
+++ next.config.js
@@ -5,26 +5,6 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!isServer) {
-      config.resolve.alias['next/dist/client/dev/error-overlay/websocket.js'] = 'next/dist/client/next.js';
-    }
-
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /next-contentlayer/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [['next/babel', { 'preset-env': { modules: 'commonjs' } }]],
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
