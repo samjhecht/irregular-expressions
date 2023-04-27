9c5b578d85d927bfe1e6db118ac4ac5fe81e911c
samjhecht@gmail.com <samjhecht@gmail.com>
2023-04-27 13:58:57 -0700

    tweaks to build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["next/dist/client/dev/error-overlay/websocket.js"] = "next/dist/client/websocket.js";
+    }
+    return config;
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
