8a014340bb64592ccd83bc0d6fb19558136111cc
samjhecht@gmail.com <samjhecht@gmail.com>
2023-04-28 08:33:47 -0700

    trying a preview build again

--- hooks/useMdxComponentCustom.tsx
+++ hooks/useMdxComponentCustom.tsx
@@ -1,27 +1,31 @@
+// hooks/useMDXComponent.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { _jsx_runtime as _jsx } from '../jsx-runtime';
+import { _jsx_runtime } from './jsx-runtime.cjs';
 
-export const getMDXComponent = (code: string, globals: object = {}): any => {
-  const scope = { React, ReactDOM, _jsx, ...globals };
+const getMDXComponent = (code, globals = {}) => {
+  const scope = { React, ReactDOM, _jsx_runtime, ...globals };
   const fn = new Function(...Object.keys(scope), code);
   return fn(...Object.values(scope)).default;
 };
 
-export const useMDXComponentCustom = (code: string, globals: object = {}): any => {
+export const useMDXComponent = (code, globals = {}) => {
   return React.useMemo(() => getMDXComponent(code, globals), [code, globals]);
 };
 
-// import { useMemo } from 'react';
-// import { _jsx_runtime as _jsx } from '../jsx-runtime.cjs';
+
+
+
+// import React from 'react';
 // import ReactDOM from 'react-dom';
+// import { _jsx_runtime as _jsx } from '../jsx-runtime';
 
 // export const getMDXComponent = (code: string, globals: object = {}): any => {
-//   const scope = { _jsx, ReactDOM, ...globals };
+//   const scope = { React, ReactDOM, _jsx, ...globals };
 //   const fn = new Function(...Object.keys(scope), code);
 //   return fn(...Object.values(scope)).default;
 // };
 
 // export const useMDXComponentCustom = (code: string, globals: object = {}): any => {
-//   return useMemo(() => getMDXComponent(code, globals), [code, globals]);
+//   return React.useMemo(() => getMDXComponent(code, globals), [code, globals]);
 // };
